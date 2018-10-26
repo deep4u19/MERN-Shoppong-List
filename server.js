7e1a4a741a5752311cdbc1bd5e6a20926a4b5851
deep4u19 <deep4u19@gmail.com>
2018-10-26 12:46:16 +0600

    make it better

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 const items=require('./routes/api/items');
 const app=express();
+const path=require('path');
 
 app.use(bodyParser.json())
 // MonGo Connection
@@ -15,7 +16,14 @@ mongoose
 // Routes
 app.use('/api/items',items);
 
-
+// Static assets in production
+if(process.env.NODE_ENV==='production'){
+// set static folder
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 const port=process.env.PORT || 6000;
 
 app.listen(port,()=>console.log(`Server running on port ${port}`));
